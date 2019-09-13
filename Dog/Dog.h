b88d9bd6f2523006d50a57e3e0cb9cf61bcb20dd
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2019-09-14 00:59:23 +0900

    Add Cocoa Touch Framework (dynamic) Dog

---
+++ Dog/Dog.h
@@ -0,0 +1,19 @@
+//
+//  Dog.h
+//  Dog
+//
+//  Created by komaji on 2019/09/14.
+//  Copyright © 2019 komaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dog.
+FOUNDATION_EXPORT double DogVersionNumber;
+
+//! Project version string for Dog.
+FOUNDATION_EXPORT const unsigned char DogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dog/PublicHeader.h>
+
+
---
+++ Dog/DogClass.swift
@@ -0,0 +1,17 @@
+//
+//  DogClass.swift
+//  Dog
+//
+//  Created by komaji on 2019/09/14.
+//  Copyright © 2019 komaji. All rights reserved.
+//
+
+import Foundation
+
+public class DogClass {
+    public init() {}
+    
+    public func say() {
+        print("わん")
+    }
+}
