1898c7fec6f633af8d5d2a4337aede8d55826291
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2019-09-14 00:10:57 +0900

    git init

---
+++ ImportAndLinkExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImportAndLinkExample
+//
+//  Created by komaji on 2019/09/14.
+//  Copyright © 2019 komaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
