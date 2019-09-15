289a74ffd5cbeccf4acac7cca7dd245c535b01db
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2019-09-15 16:49:11 +0900

    Add Cocoa Touch Static Library Cat

---
+++ Cat/Cat.swift
@@ -0,0 +1,15 @@
+//
+//  CatClass.swift
+//  CatClass
+//
+//  Created by komaji on 2019/09/15.
+//  Copyright © 2019 komaji. All rights reserved.
+//
+
+public class CatClass {
+    public init() {}
+
+    public func say() {
+        print("にゃー")
+    }
+}
